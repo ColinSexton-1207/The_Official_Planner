97ff824540be5c74d1ff6908baba1ff4567ca8b3
Colin Sexton <colin.sexton.1207@gmail.com>
2022-02-17 17:28:39 -0600

    Boilerplate update. Setting up Events, while thinking about logic of each class

---
+++ src/events/Classes.java
@@ -0,0 +1,12 @@
+/* 
+ * Version 0.0.1a
+ * Developer: Colin Sexton
+ * Updated: 02/17/2022
+ * One of the main scheduled events, implements the Events interface.
+ */
+
+package events;
+
+public class Classes implements Event {
+
+}
---
+++ src/events/Event.java
@@ -0,0 +1,12 @@
+/* 
+ * Version 0.0.1a
+ * Developer: Colin Sexton
+ * Updated: 02/17/2022
+ * Inteface that will shape the main events that will be added to the calendar.
+ */
+
+package events;
+
+abstract interface Event {
+
+}
